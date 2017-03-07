54083bc6c7e0a77dc57447d7fb528aac259a7b8f
bobzz-zone <bobzz.zone@gmail.com>
2017-03-07 14:22:08 +0700

    fix get

--- massaction/mass_action/doctype/quotation_to_order/quotation_to_order.js
+++ massaction/mass_action/doctype/quotation_to_order/quotation_to_order.js
@@ -5,22 +5,4 @@ frappe.ui.form.on('Quotation To Order', {
 	refresh: function(frm) {
 
 	},
-	get_quotation: function(frm) {
-		frappe.call({
-			doc: frm.doc,
-			method: "get_quotation",
-			callback: function(r) {
-				refresh_field("quotation_list");
-			}
-		});
-	},
-	get_item: function(frm) {
-		frappe.call({
-			doc: frm.doc,
-			method: "get_item",
-			callback: function(r) {
-				refresh_field("items");
-			}
-		});
-	},
 });
--- massaction/mass_action/doctype/quotation_to_order/quotation_to_order.py
+++ massaction/mass_action/doctype/quotation_to_order/quotation_to_order.py
@@ -7,21 +7,21 @@ import frappe
 from frappe.model.document import Document
 
 class QuotationToOrder(Document):
-	def get_quotation(self):
+	def get_quotations(self):
 		where = """ docstatus=1 and status = "Open" and customer = "{}" """.format(self.customer)
 		if self.from_date :
-			where = """{} and transaction_date > "{}" """.format(self.from_date)
+			where = """{} and transaction_date > "{}" """.format(where,self.from_date)
 		if self.to_date :
-			where = """{} and transaction_date < "{}" """.format(self.to_date)
+			where = """{} and transaction_date < "{}" """.format(where,self.to_date)
 		quotations=frappe.db.sql("select name, transaction_date,grand_total from tabQuotation where {}".format(where),as_list=1)
 		self.set("quotation_list", [])
 		for r in quotations:
 			pp_so = self.append('quotation_list', {})
-			pp_so.quotation = r['name']
-			pp_so.date = r['transaction_date']
-			pp_so.grand_total = r['grand_total']
+			pp_so.quotation = r[0]
+			pp_so.date = r[1]
+			pp_so.total = r[2]
 
-	def get_item(self):
+	def get_items(self):
 		so_list = [d.quotation for d in self.get('quotation_list') if d.quotation]
 		if not qo_list:
 			msgprint(_("Please enter Quotation in the above table"))
@@ -33,9 +33,11 @@ class QuotationToOrder(Document):
 		self.set("items", [])
 		for r in quotations:
 			pp_so = self.append('items', {})
-			pp_so.item = r['item_code']
-			pp_so.quotation = r['parent']
-			pp_so.qty = r['qty']
-			pp_so.rate = r['rate']
-			pp_so.total = r['amount']
-			pp_so.quotation_item = r['name']
+			pp_so.item = r[0]
+			pp_so.quotation = r[1]
+			pp_so.qty = r[2]
+			pp_so.rate = r[3]
+			pp_so.total = r[4]
+			pp_so.quotation_item = r[5]
+	def create_order(self):
+		
